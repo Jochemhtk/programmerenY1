35fdd024e39c7dc4ac7d01ddcc25fb869969ae86
Jochemhtk <54931971+Jochemhtk@users.noreply.github.com>
2023-10-06 16:13:25 +0200

    Add files via upload

--- week4e.js
+++ week4e.js
@@ -1,1 +1,22 @@
+var readlineSync = require('readline-sync');
+var nee = "nee";
+var Nee = "Nee";
 
+console.clear();
+var score = 0;
+var answer = readlineSync.question(`Wilt u klikken?\n(enter of nee)\n`);
+
+while (answer === ``){
+    console.log(`Score: ${score}\n`);
+    var answer = readlineSync.question(`Wil je klikken?\n`);
+    score++;
+    console.clear();
+}
+if (answer === nee) {
+    console.clear();
+    console.log(`Score: ${score}\n`);
+}
+else if (answer === Nee) {
+    console.clear();
+    console.log(`Score: ${score}\n`);
+}
