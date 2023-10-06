947bf6c972fef85ba003dbaef68a4cbd26e29a34
Jochemhtk <54931971+Jochemhtk@users.noreply.github.com>
2023-10-06 13:02:16 +0200

    Update week4e.js

--- week4e.js
+++ week4e.js
@@ -1,10 +1,1 @@
-function multiplicationTable(number){
-    for(let i = 1; i <= 10; i++) {
-        const result = i * number;
-        console.log(`${number} * ${i} = ${result}`);
-    }
-}
 
-for(let i = 1; i <= 50; i++) {
-    multiplicationTable(i)
-}
